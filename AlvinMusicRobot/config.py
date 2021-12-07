40ec81c94e9e3d7c844a36637ffbd584ab0acb7d
bismillah,fahri ganteng <90828420+bismillahfahriganteng@users.noreply.github.com>
2021-12-07 13:40:18 +0800

    Update config.py

--- AlvinMusicRobot/config.py
+++ AlvinMusicRobot/config.py
@@ -1,20 +1,3 @@
-# AlvinMusicRobot- Telegram bot project
-# Copyright (C) 2021  Roj Serbest
-# Copyright (C) 2021  Inuka Asith
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
-
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
-# Modified by alvin_junior
-
 import os
 from os import path
 from os import getenv
@@ -29,7 +12,7 @@ BOT_TOKEN = getenv("BOT_TOKEN")
 BOT_NAME = getenv("BOT_NAME")
 UPDATES_CH = getenv("UPDATES_CHANNEL", "alvin_image_editor")
 UPDATES_MODE = getenv("UPDATES_MODE", "Updates")
-BG_IMAGE = getenv("BG_IMAGE", "https://telegra.ph/file/b623ab3cd8caef9efff2b.jpg")
+BG_IMAGE = getenv("BG_IMAGE", "https://telegra.ph/file/835ec872ee0aa9b8de348.jpg")
 admins = {}
 API_ID = int(getenv("API_ID", ""))
 API_HASH = getenv("API_HASH")
